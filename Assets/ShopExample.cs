65fbcd686fe95a4390cfca9be2e0fa3db8f8506c
Danila <64286216+Danila01010101@users.noreply.github.com>
2025-01-24 19:06:30 +0300

    Shop example

---
+++ Assets/ShopExample.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class ShopExample : MonoBehaviour
+{
+    [SerializeField] private GameObject gnome;
+
+    public static Action<SkinData> GnomeSkinChanged;
+
+    private void Awake()
+    {
+	    ResourcesCounter.Instance.Data.GemsAmount += 1000;
+	    var skin = new SkinData(gnome, 10);
+    }
+
+    public void BuySkin(SkinData skin)
+    {
+	    if (skin.Cost > ResourcesCounter.Instance.Data.GemsAmount)
+		    return;
+	    
+	    ResourcesCounter.Instance.Data.GemsAmount -= skin.Cost;
+	    GnomeSkinChanged?.Invoke(skin);
+    }
+
+    public class SkinData
+    {
+	    private GameObject skin;
+	    private Material skinMaterial;
+	    public int Cost { get; private set; }
+
+	    public SkinData(GameObject skin, int cost)
+	    {
+		    this.skin = skin;
+		    Cost = cost;
+	    }
+    }
+}
