6049c4546fdd837b577a2d23734821e14feea526
mivoky <66131852+mivoky@users.noreply.github.com>
2025-02-09 22:23:15 +0300

    SpellCast RandomMode

--- Assets/AOneButtonDefence/Scripts/SpellCast/SpellCast.cs
+++ Assets/AOneButtonDefence/Scripts/SpellCast/SpellCast.cs
@@ -1,30 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 // Скрипт выдает спелы
 //Скрипт кастует 
 public class SpellCast : MonoBehaviour
 {
-    public List<SpellStorage> SpellStorage;
-    public int CurrentChose;
+    public bool RandomSpellMode;
     public bool Active;
 
+    public List<SpellStorage> SpellStorage;
+    public int CurrentChose;
+    [Header("Graphic")]
+    public Image CurrentSpell;
+    public Image NextSpell;
+
     private IInput input = new DesctopInput(0);
+    private List<SpellData> randomSpell = new List<SpellData>();
 
     public void Initialize(IInput input)
     {
+        GameBattleState.BattleWon += AddNextSpell;
         this.input = input;
-        input.Clicked += Cast;
+        if (RandomSpellMode == true)
+        {
+            input.Clicked += RandomModeCast;
+            InitilizeRandomMode();
+        }
+        else
+        {
+            input.Clicked += Cast;
+        }
         Physics.queriesHitTriggers = false;
     }
-    private void Start()
+
+    private void InitilizeRandomMode()
     {
-        Initialize(input);
+        randomSpell.Add(SpellStorage[Random.Range(0, SpellStorage.Count)].Spell);
+        randomSpell.Add(SpellStorage[Random.Range(0, SpellStorage.Count)].Spell);
     }
+
     private void Update()
     {
         input.Update();
     }
+
     private void Cast(Vector2 position)
     {
         Ray ray = Camera.main.ScreenPointToRay(position);
@@ -41,6 +61,29 @@ public class SpellCast : MonoBehaviour
         {
             Active = false;
         }
+    }
+
+    private void RandomModeCast(Vector2 position)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        RaycastHit hit;
+        if (randomSpell[0] == null) 
+        {
+            Debug.Log("Заклинание отсутсвует в первом слоте!");
+            return;
+        }
+        if (Active == true && Physics.Raycast(ray, out hit)) 
+        {
+            GameObject spell = Instantiate(randomSpell[0].BaseMagicCircle, new Vector3(hit.point.x, 1, hit.point.z), Quaternion.identity);
+            spell.GetComponent<Spell>().Initialize(randomSpell[0]);
+            randomSpell[0] = null;
+        }
+    }
+
+    public void AddNextSpell()
+    {
+        randomSpell[0] = randomSpell[1];
+        randomSpell[1] = SpellStorage[Random.Range(0, SpellStorage.Count)].Spell;
     }
 
     private void OnDestroy()
