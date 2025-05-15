5bcd9800b5ce05e261559ce3a0d1fd7f6812aa01
mivoky <66131852+mivoky@users.noreply.github.com>
2025-05-15 23:52:47 +0300

    AddWaypointToMainScene
    Добавлены метки на рыцарей.
    
    Ошибка: скрытые метки не удаляются, требуется каким-то образом получить компонент WaypointMarkerUI и удалять его при уничтожение WaypointTarget

--- Assets/AOneButtonDefence/Scripts/Enemies/Knight.cs
+++ Assets/AOneButtonDefence/Scripts/Enemies/Knight.cs
@@ -1,14 +1,23 @@
 using System;
 using UnityEngine;
+using WrightAngle.Waypoint;
 
+[RequireComponent(typeof(WaypointTarget))]
 public class Knight : FightingUnit
 {
     public static Action<Vector3, int> OnEnemyDeath;
-    
+    private WaypointTarget waypoint;
+    private void Start()
+    {
+        waypoint = GetComponent<WaypointTarget>();
+        waypoint.ActivateWaypoint();
+        WaypointUIManager.OnWaypointTargetAdd.Invoke(waypoint);
+    }
     protected override void Die()
     {
         base.Die();
         int gemsAmount = UnityEngine.Random.Range(characterStats.MinGemSpawn, characterStats.MaxGemSpawn);
         EnemyDeathManager.Instance.NotifyEnemyDeath(transform.position, gemsAmount);
+        waypoint.DestroyWaypoint();
     }
 }
