dd6687f0833d9d92c3c9d439a8ae6c4cfa3391a3
Danila <64286216+Danila01010101@users.noreply.github.com>
2025-09-03 04:29:12 +0300

    Health size scale for camera zoom

--- Assets/AOneButtonDefence/Scripts/UI/PlayerHealthbar.cs
+++ Assets/AOneButtonDefence/Scripts/UI/PlayerHealthbar.cs
@@ -8,18 +8,36 @@ public class PlayerHealthbar : MonoBehaviour
     [SerializeField] private int healthIconsAmount = 10;
     [SerializeField] private float minFill = 0.1f;
     [SerializeField] private float maxFill = 0.98f;
+    [SerializeField] private float scaleFactor = 1f;
 
+    private Camera camera;
     private Health health;
 
     public void Initialize(Health health, Camera camera = null)
     {
         if (camera != null)
+        {
+            this.camera = camera;
             billboardCanvas.SetCamera(camera);
+        }
+        else
+        {
+            this.camera = Camera.main;
+        }
 
         this.health = health;
         SetHealth(health.Amount, health.Amount);
         health.DamageReceived += SetHealth;
     }
+    
+    private void LateUpdate()
+    {
+        if (camera == null) return;
+
+        float distance = Vector3.Distance(transform.position, camera.transform.position);
+        transform.localScale = Vector3.one * distance * scaleFactor;
+    }
+
 
     private void SetHealth(float current, float max)
     {
