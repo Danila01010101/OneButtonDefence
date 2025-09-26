5b99b293eb2f939a4497223609e12c1b96b6e671
Danila <64286216+Danila01010101@users.noreply.github.com>
2025-09-26 16:08:40 +0300

    Web build wave generation fixed

--- Assets/AOneButtonDefence/Scripts/WaveGenerator.cs
+++ Assets/AOneButtonDefence/Scripts/WaveGenerator.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class WaveGenerator
 {
@@ -22,94 +22,99 @@ public class WaveGenerator
         public List<RuntimeWaveData> Waves;
     }
 
-    public static async Task<RuntimeWavesParameters> GenerateWaves(
+    public static Task<RuntimeWavesParameters> GenerateWaves(
         BattleWavesParameters source, int countWaves)
     {
-        return await Task.Run(() =>
+#if UNITY_WEBGL
+        return Task.FromResult(GenerateWavesSync(source, countWaves));
+#else
+        return Task.Run(() => GenerateWavesSync(source, countWaves));
+#endif
+    }
+
+    private static RuntimeWavesParameters GenerateWavesSync(
+        BattleWavesParameters source, int countWaves)
+    {
+        var runtime = new RuntimeWavesParameters
         {
-            var runtime = new RuntimeWavesParameters
+            Waves = new List<RuntimeWaveData>()
+        };
+
+        for (int waveIndex = 0; waveIndex < countWaves; waveIndex++)
+        {
+            float waveSpawnInterval = (source.waves.Count > 0)
+                ? source.waves[0].spawnInterval
+                : 3f;
+
+            var newWave = new RuntimeWaveData
             {
-                Waves = new List<RuntimeWaveData>()
+                SpawnInterval = waveSpawnInterval,
+                EnemiesToSpawn = new List<RuntimeEnemySpawnData>()
             };
 
-            for (int waveIndex = 0; waveIndex < countWaves; waveIndex++)
+            if (source.waves.Count > 0)
             {
-                float waveSpawnInterval = (source.waves.Count > 0) 
-                    ? source.waves[0].spawnInterval 
-                    : 3f;
-
-                var newWave = new RuntimeWaveData
+                foreach (var enemy in source.waves[0].enemiesToGenerate)
                 {
-                    SpawnInterval = waveSpawnInterval,
-                    EnemiesToSpawn = new List<RuntimeEnemySpawnData>()
-                };
+                    if (!enemy.isEnabled) continue;
+                    if (waveIndex + 1 < enemy.startWave) continue;
 
-                if (source.waves.Count > 0)
-                {
-                    foreach (var enemy in source.waves[0].enemiesToGenerate)
+                    int baseAmount = 0;
+
+                    switch (enemy.growthType)
                     {
-                        if (!enemy.isEnabled) continue;
-                        if (waveIndex + 1 < enemy.startWave) continue;
+                        case BattleWavesParameters.GrowthType.Linear:
+                            baseAmount = enemy.startAmount +
+                                (waveIndex + 1 - enemy.startWave) * enemy.growthPerWave;
+                            break;
 
-                        int baseAmount = 0;
-
-                        switch (enemy.growthType)
-                        {
-                            case BattleWavesParameters.GrowthType.Linear:
-                                baseAmount = enemy.startAmount +
-                                    (waveIndex + 1 - enemy.startWave) * enemy.growthPerWave;
-                                break;
-
-                            case BattleWavesParameters.GrowthType.Exponential:
-                                baseAmount = (int)Math.Round(
-                                    enemy.startAmount * (float)Math.Pow(
-                                        1 + enemy.growthPerWave / 100f,
-                                        waveIndex + 1 - enemy.startWave));
-                                break;
-                        }
-
-                        if (waveIndex + 1 > enemy.maxWave)
-                            baseAmount = Math.Max(enemy.startAmount, 1);
-
-                        baseAmount = Math.Max(baseAmount, 1);
-
-                        if (enemy.randomVariancePercent > 0f)
-                        {
-                            float variance = baseAmount * (enemy.randomVariancePercent / 100f);
-                            baseAmount = (int)Math.Round(
-                                baseAmount + RandomRange(-variance, variance));
-                            baseAmount = Math.Max(baseAmount, 1);
-                        }
-
-                        float spawnInterval = (enemy.customSpawnInterval > 0) 
-                            ? enemy.customSpawnInterval 
-                            : waveSpawnInterval;
-
-                        newWave.EnemiesToSpawn.Add(new RuntimeEnemySpawnData
-                        {
-                            EnemyPrefab = enemy.EnemyPrefab,
-                            Amount = baseAmount,
-                            SpawnInterval = spawnInterval
-                        });
+                        case BattleWavesParameters.GrowthType.Exponential:
+                            baseAmount = Mathf.RoundToInt(
+                                enemy.startAmount * Mathf.Pow(
+                                    1 + enemy.growthPerWave / 100f,
+                                    waveIndex + 1 - enemy.startWave));
+                            break;
                     }
-                }
 
-                runtime.Waves.Add(newWave);
+                    if (waveIndex + 1 > enemy.maxWave)
+                        baseAmount = Mathf.Max(enemy.startAmount, 1);
+
+                    baseAmount = Mathf.Max(baseAmount, 1);
+
+                    if (enemy.randomVariancePercent > 0f)
+                    {
+                        float variance = baseAmount * (enemy.randomVariancePercent / 100f);
+                        baseAmount = Mathf.RoundToInt(
+                            baseAmount + RandomRange(-variance, variance));
+                        baseAmount = Mathf.Max(baseAmount, 1);
+                    }
+
+                    float spawnInterval = (enemy.customSpawnInterval > 0)
+                        ? enemy.customSpawnInterval
+                        : waveSpawnInterval;
+
+                    newWave.EnemiesToSpawn.Add(new RuntimeEnemySpawnData
+                    {
+                        EnemyPrefab = enemy.EnemyPrefab,
+                        Amount = baseAmount,
+                        SpawnInterval = spawnInterval
+                    });
+                }
             }
 
-            return runtime;
-        });
+            runtime.Waves.Add(newWave);
+        }
+
+        return runtime;
     }
-    
-    private static Random sysRand = new System.Random();
 
     private static int RandomRange(int min, int max)
     {
-        return sysRand.Next(min, max);
+        return UnityEngine.Random.Range(min, max);
     }
 
     private static float RandomRange(float min, float max)
     {
-        return (float)(sysRand.NextDouble() * (max - min) + min);
+        return UnityEngine.Random.Range(min, max);
     }
 }
