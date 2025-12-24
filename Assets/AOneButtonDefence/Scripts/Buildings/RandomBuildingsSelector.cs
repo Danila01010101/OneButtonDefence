2ab49ba4e16ffefa88f19a1122fb6d95b9da3ee1
Danila <64286216+Danila01010101@users.noreply.github.com>
2025-12-24 20:44:34 +0300

    Buildings spawn rate fixes

--- Assets/AOneButtonDefence/Scripts/Buildings/RandomBuildingsSelector.cs
+++ Assets/AOneButtonDefence/Scripts/Buildings/RandomBuildingsSelector.cs
@@ -1,58 +1,97 @@
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class RandomBuildingsSelector
 {
-    private readonly List<BasicBuildingData> allBuildings;
+    private List<BasicBuildingData> allBuildings;
+    private List<BasicBuildingData> randomizableBuildings;
+    private List<BasicBuildingData> nonRandomizableBuildings;
 
     public RandomBuildingsSelector(List<BasicBuildingData> buildings)
     {
         allBuildings = buildings;
+        nonRandomizableBuildings = allBuildings.Where(b => !b.IsRandomisable).ToList();
+        randomizableBuildings = allBuildings.Where(b => b.IsRandomisable).ToList();
     }
 
     public List<BasicBuildingData> GetSelection(int count)
     {
-        List<BasicBuildingData> pool = new List<BasicBuildingData>();
+        if (count <= 0)
+            return new List<BasicBuildingData>();
 
-        foreach (var building in allBuildings)
+        var result = new List<BasicBuildingData>();
+
+        if (nonRandomizableBuildings.Count > 0)
         {
-            if (!building.IsRandomisable || building.SpawnChance <= 0f)
-                continue;
-
-            pool.Add(building);
+            var weightedNonRandom = GetWeightedSelection(nonRandomizableBuildings, 1);
+            if (weightedNonRandom.Count > 0)
+                result.Add(weightedNonRandom[0]);
         }
 
-        List<BasicBuildingData> result = new List<BasicBuildingData>();
-
-        for (int i = 0; i < count; i++)
+        if (result.Count < count)
         {
-            if (pool.Count == 0)
-                break;
+            var allAvailable = new List<BasicBuildingData>();
+            allAvailable.AddRange(nonRandomizableBuildings.Where(b => !result.Contains(b)));
+            allAvailable.AddRange(randomizableBuildings);
 
-            var chosen = GetRandomWeighted(pool);
-            result.Add(chosen);
+            int remainingSlots = count - result.Count;
+            var weightedRandom = GetWeightedSelection(allAvailable, remainingSlots);
+            result.AddRange(weightedRandom);
+        }
 
-            pool.Remove(chosen);
+        return ShuffleList(result);
+    }
+
+    private List<BasicBuildingData> GetWeightedSelection(List<BasicBuildingData> buildings, int count)
+    {
+        if (buildings.Count == 0 || count <= 0)
+            return new List<BasicBuildingData>();
+
+        var result = new List<BasicBuildingData>();
+        var availableBuildings = new List<BasicBuildingData>(buildings);
+
+        for (int i = 0; i < count && availableBuildings.Count > 0; i++)
+        {
+            float totalWeight = 0f;
+            
+            foreach (var building in availableBuildings)
+            {
+                totalWeight += GetSpawnWeight(building);
+            }
+
+            float randomPoint = Random.Range(0f, totalWeight);
+            float currentWeight = 0f;
+
+            foreach (var building in availableBuildings)
+            {
+                currentWeight += GetSpawnWeight(building);
+                if (currentWeight >= randomPoint)
+                {
+                    result.Add(building);
+                    availableBuildings.Remove(building);
+                    break;
+                }
+            }
         }
 
         return result;
     }
 
-    private BasicBuildingData GetRandomWeighted(List<BasicBuildingData> pool)
+    private float GetSpawnWeight(BasicBuildingData building)
     {
-        float totalWeight = 0f;
-        foreach (var b in pool)
-            totalWeight += b.SpawnChance;
+        return building.SpawnChance;
+    }
 
-        float rand = UnityEngine.Random.value * totalWeight;
-        float cumulative = 0f;
-
-        foreach (var b in pool)
+    private List<BasicBuildingData> ShuffleList(List<BasicBuildingData> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
         {
-            cumulative += b.SpawnChance;
-            if (rand <= cumulative)
-                return b;
+            int j = Random.Range(0, i + 1);
+            var temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
         }
-
-        return pool[pool.Count - 1];
+        return list;
     }
 }
